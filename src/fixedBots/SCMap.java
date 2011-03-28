9b751b2f4ac77f2355797c9685fef8ae06db075f
Paul <paul.ruan@gmail.com>
2011-03-28 01:55:04 -0700

    forgot to save

--- src/fixedBots/SCMap.java
+++ src/fixedBots/SCMap.java
@@ -1,9 +1,12 @@
 package fixedBots;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.bwapi.proxy.model.Game;
+import org.bwapi.proxy.model.Player;
 import org.bwapi.proxy.model.ROUnit;
 import org.bwapi.proxy.model.TilePosition;
 
@@ -13,6 +16,7 @@ public class SCMap {
 	private Set<TilePosition> startSpots;
 	private Set<ROUnit> gasSpots; //use to figure out possible expansion locations
 	private Set<ROUnit> buildings; //buildings seen
+	private Player self;
 	
 	@SuppressWarnings("unchecked")
 	public SCMap(){
@@ -21,6 +25,7 @@ public class SCMap {
 		startSpots = Game.getInstance().getStartLocations();
 		gasSpots = (Set<ROUnit>) Game.getInstance().getGeysers();
 		buildings = new HashSet<ROUnit>();
+		self = Game.getInstance().self();
 	}
 	
 	public int getHeight(){ return height;}
@@ -28,7 +33,21 @@ public class SCMap {
 	public Set<TilePosition> getStartSpots(){ return startSpots;}
 	public Set<ROUnit> getGasSpots(){ return gasSpots;}
 	
-	public Set<ROUnit> getBuildings(){return buildings;}
+	public Set<ROUnit> getBuildings(){
+		return buildings;
+	}
+	
+	public void refresh(){
+		/*
+		List<ROUnit> dNE = new ArrayList<ROUnit>();
+		for(ROUnit unit: buildings){
+			if(self.canSeeUnitAtPosition(unit.getType(),unit.getLastKnownPosition()) && !unit.isVisible())
+				dNE.add(unit);
+		}
+		for(ROUnit d: dNE){
+			buildings.remove(d);
+		}*/
+	}
 	public void addBuilding(ROUnit u){buildings.add(u);}
 	public void removeBuilding(ROUnit u){buildings.remove(u);}
 }
